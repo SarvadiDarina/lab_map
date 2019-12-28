f2428c61f1e319a3b227af1af919b93a2a3bf30a
SarvadiDarina <56667667+SarvadiDarina@users.noreply.github.com>
2019-12-28 08:07:41 +0200

    зміни

--- task2.py
+++ task2.py
@@ -2,7 +2,7 @@
 #перпендикулярними.
 vect1 = [float(input('Координати вектора 1 : ')) for x in range(3)]
 vect2 = [float(input('Координати вектора 2 : ')) for x in range(3)]
-result = list(map(lambda x1, x2: x1 + x2, vect1, vect2))
+result = list(map(lambda x1, x2: x1 * x2, vect1, vect2))
 if sum(result) == 0:
     print('перпендикулярні')
 else:
